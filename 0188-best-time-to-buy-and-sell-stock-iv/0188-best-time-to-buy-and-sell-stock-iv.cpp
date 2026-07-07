0390260879ed9532f928ef4afb40f9b72dd885d3
Aryan kumar <aryankrbjunitp@gmail.com>
2026-07-07 14:43:02 +0530

    Time: 12 ms (35.48%), Space: 15.1 MB (69.89%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,29 +1,22 @@
 class Solution {
 public:
     int dp[1001][2][101];
-
-    int solve(int i, int buy, int mcount, vector<int>& prices) {
-        if (i == prices.size() || mcount == 0)
+    int mp(int i, int buy, int mc, vector<int>& prices) {
+        if (i == prices.size() || mc == 0)
             return 0;
-
-        if (dp[i][buy][mcount] != -1)
-            return dp[i][buy][mcount];
-
-        int profit = 0;
-
+        if(dp[i][buy][mc]!=-1) return dp[i][buy][mc];
+        int profit =0;
         if (buy) {
-            profit = max(-prices[i] + solve(i + 1, 0, mcount, prices),
-                         solve(i + 1, 1, mcount, prices));
+            profit = max(-prices[i] + mp(i + 1, 0, mc, prices),
+                                 0 + mp(i + 1, 1, mc, prices));
         } else {
-            profit = max(prices[i] + solve(i + 1, 1, mcount - 1, prices),
-                         solve(i + 1, 0, mcount, prices));
+            profit = max(prices[i] + mp(i + 1, 1, mc - 1, prices),
+                                 0 + mp(i + 1, 0, mc, prices));
         }
-
-        return dp[i][buy][mcount] = profit;
+        return dp[i][buy][mc]  = profit;
     }
-
     int maxProfit(int k, vector<int>& prices) {
         memset(dp, -1, sizeof(dp));
-        return solve(0, 1, k, prices);
+        return mp(0, 1, k, prices);
     }
 };
