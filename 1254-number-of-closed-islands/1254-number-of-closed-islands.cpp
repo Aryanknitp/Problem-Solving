d2d31776ae17d13799a60307556adc394961a29d
Aryan kumar <aryankrbjunitp@gmail.com>
2026-07-17 13:26:20 +0530

    Time: 0 ms (100%), Space: 14.2 MB (28.95%) - LeetHub

--- 1254-number-of-closed-islands/1254-number-of-closed-islands.cpp
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.cpp
@@ -1,38 +1,70 @@
 class Solution {
 public:
-    void solve(vector<vector<int>>& grid, vector<vector<int>>& vis, int i,
-               int j) {
-        int n = grid.size() , m= grid[0].size();
-        vis[i][j] = 1;
-        vector<int> dr = {-1, 0, 1, 0};
-        vector<int> dc = {0, 1, 0, -1};
-        for (int k = 0; k < 4; k++) {
-            int nr = i + dr[k];
-            int nc = j + dc[k];
-            if ( nr>=0 && nr<n&& nc>=0 && nc<m && !vis[nr][nc] && grid[nr][nc] == 0) {
-                solve(grid, vis, nr, nc);
-            }
-        }
-    }
+class dsu{
+     public: vector<int>p;
+     dsu(int n) {
+         p.resize(n);
+         for(int i=0;i<n;i++) p[i] =i;
+     }
+     int find(int i) {
+        if(p[i]==i) return i;
+       return  p[i] = find(p[i]);
+     }
+     void unite(int i,int j) {
+         int ri= find(i);
+         int rj = find(j);
+         if(ri!=rj) {
+            p[ri] = rj;
+         }
+     }
+};
+
     int closedIsland(vector<vector<int>>& grid) {
-        int n = grid.size(), m = grid[0].size();
-        vector<vector<int>> vis(n, vector<int>(m, 0));
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (i == 0 || i == n - 1 || j == 0 || j == m - 1) 
-                    if (!vis[i][j] && grid[i][j] == 0) 
-                          solve(grid, vis, i, j);
-            }
-        }
-        int cnt = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (!vis[i][j] && grid[i][j] == 0) {
-                    cnt++;
-                    solve(grid, vis, i, j);
+        int n = grid.size() ;
+        int m = grid[0].size();
+        int db = n*m;
+        dsu ds(db+1);
+        // step1 unite the border of grid
+        for(int i=0;i<n;i++) {
+            for(int j=0;j<m;j++) {
+                if(grid[i][j]==0){
+                     int ci = i*m+j;
+                     if(i==0 || i==n-1|| j==0 || j==m-1){
+                        ds.unite(ci,db);
+                     }
                 }
             }
         }
-        return cnt;
+        for(int i=0;i<n;i++) {
+             for(int j=0;j<m;j++) {
+                 if(grid[i][j]==0) {
+                    //   right
+                      int ci = i*m+j;
+                      if(j+1<m && grid[i][j+1]==0){
+                        ds.unite(ci,i*m+(j+1));
+                      }
+                    //   down
+                      if(i+1<n && grid[i+1][j]==0){
+                         ds.unite(ci,(i+1)*m+j);
+                      }
+                 }
+             }
+        }
+        unordered_set<int> closedIslandRoots;
+        int borderRoot = ds.find(db);
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == 0) {
+                    int cellRoot = ds.find(i * m + j);
+                    // If this land cell group does not touch the border, track its root
+                    if (cellRoot != borderRoot) {
+                        closedIslandRoots.insert(cellRoot);
+                    }
+                }
+            }
+        }
+        
+        return closedIslandRoots.size();
     }
 };
